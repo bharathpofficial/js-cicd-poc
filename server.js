e105a16bd37f9e663ba7b19a6bf1ddd1a4a28c65
bharath.p <bharathbhd3@gmail.com>
2026-02-02 23:04:36 +0530

    Fix server for Jest/supertest compatibility

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => res.send('Hello CI/CD!'));
-app.listen(3000, () => console.log('Server on 3000'));
+
+// Export app for testing (no auto-listen)
+module.exports = app;
 
--- server.test.js
+++ server.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
 const app = require('./server');
 
-test('GET / responds with Hello CI/CD', async () => {
-  const res = await request(app).get('/');
-  expect(res.status).toBe(200);
-  expect(res.text).toBe('Hello CI/CD!');
+describe('API Tests', () => {
+  test('GET / responds with Hello CI/CD', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('Hello CI/CD!');
+  });
 });
 
