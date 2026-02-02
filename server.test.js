d0c74919d011d31decd3ab164272c961fd084a01
bharath.p <bharathbhd3@gmail.com>
2026-02-02 22:52:38 +0530

    Initial CI/CD POC

---
+++ server.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('./server');
+
+test('GET / responds with Hello CI/CD', async () => {
+  const res = await request(app).get('/');
+  expect(res.status).toBe(200);
+  expect(res.text).toBe('Hello CI/CD!');
+});
+
